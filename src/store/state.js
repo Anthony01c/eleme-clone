86335967950870ec7337d0af17170bbb60685fb8
Lukas <yangmingdada@gmail.com>
2021-06-23 15:47:00 +0800

    首页相关完成

--- src/store/state.js
+++ src/store/state.js
@@ -1,7 +1,7 @@
 /*
 * 包含n个状态属性的对象*/
 export default {
-    geohash:'40.10038,116.36867',//经纬度
+    geohash:'30.62649,103.667',//经纬度
     address:{},//地址总对象
     categorys:[],//分类数组
     shops:[]//商家数组
