7194aebabd4db96ba59fb97dde74793cf5178600
Lukas <yangmingdada@gmail.com>
2021-06-24 14:08:36 +0800

    完成登录页验证码显示隐藏功能

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -2,28 +2,29 @@
   <section class="loginContainer">
     <div class="loginInner">
       <div class="login_header">
-        <h2 class="login_logo">硅谷外卖</h2>
+        <h2 class="login_logo">饿死了么</h2>
         <div class="login_header_title">
-          <a href="javascript:;" class="on">短信登录</a>
-          <a href="javascript:;">密码登录</a>
+          <a href="javascript:;" :class="{on: isShowSms}" @click="isShowSms = true">短信登录</a>
+          <a href="javascript:;" :class="{on: !isShowSms}" @click="isShowSms = false">密码登录</a>
         </div>
       </div>
       <div class="login_content">
         <form>
-          <div class="on">
+          <div :class="{on: isShowSms}">
             <section class="login_message">
-              <input type="tel" maxlength="11" placeholder="手机号">
-              <button disabled="disabled" class="get_verification">获取验证码</button>
+              <input type="tel" maxlength="11" placeholder="手机号" v-model="phone">
+              <button :disabled="!isRightPhone" class="get_verification"
+                      :class="{right_phone_number: isRightPhone}" @click.prevent="sendCode">获取验证码</button>
             </section>
             <section class="login_verification">
               <input type="tel" maxlength="8" placeholder="验证码">
             </section>
             <section class="login_hint">
-              温馨提示：未注册硅谷外卖帐号的手机号，登录时将自动注册，且代表已同意
+              温馨提示：未注册饿死了么帐号的手机号，登录时将自动注册，且代表已同意
               <a href="javascript:;">《用户服务协议》</a>
             </section>
           </div>
-          <div>
+          <div :class="{on: !isShowSms}">
             <section>
               <section class="login_message">
                 <input type="tel" maxlength="11" placeholder="手机/邮箱/用户名">
@@ -54,7 +55,24 @@
 
 <script>
 export default {
-  name: "Login"
+  name: "Login",
+  data(){
+    return {
+      isShowSms: true,
+      phone:''
+    }
+  },
+  computed:{
+    //简单处理是否为一个手机号
+    isRightPhone(){
+      return /^1\d{10}$/.test(this.phone)
+    }
+  },
+  methods:{
+    sendCode(){
+      alert('hello')
+    }
+  }
 }
 </script>
 
@@ -119,6 +137,8 @@ export default {
               color #ccc
               font-size 14px
               background transparent
+              &.right_phone_number
+               color #0a0a0a
           .login_verification
             position relative
             margin-top 16px
